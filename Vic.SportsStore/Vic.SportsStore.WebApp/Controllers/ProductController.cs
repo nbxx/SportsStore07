4221215450205f0e84435aa5a75145c2c8681d41
GY <GY>
2020-11-15 18:33:52 -0500

    1 [CHG] add paging;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -25,11 +26,27 @@ namespace Vic.SportsStore.WebApp.Controllers
             // V -> View
             // C -> ProductController
 
-            var model = ProductsRepository
+            //var model = ProductsRepository
+            //    .Products
+            //    .OrderBy(p => p.ProductId)
+            //    .Skip((page - 1) * PageSize)
+            //    .Take(PageSize);
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
                 .Products
                 .OrderBy(p => p.ProductId)
                 .Skip((page - 1) * PageSize)
-                .Take(PageSize);
+                .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
 
             return View(model);
         }
