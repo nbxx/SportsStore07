99cb4b6cb5b403058fa8bb513672161542408151
GY <GY>
2020-11-22 17:02:37 -0500

    1 [CHG] add cart;

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
+using Vic.SportsStore.WebApp.Models;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IProductsRepository ProductsRepository { get; set; }
+            = new EFProductRepository();
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = ProductsRepository
+                .Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = ProductsRepository
+                .Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+
+}
