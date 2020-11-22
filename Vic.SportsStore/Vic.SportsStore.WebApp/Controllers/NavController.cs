8384f602d98c72efe8314cb35b4f7a8c3d99826b
GY <GY>
2020-11-22 16:17:47 -0500

    1 [CHG] add nav menu;

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class NavController : Controller
+    {
+        public IProductsRepository ProductsRepository { get; set; }
+            = new EFProductRepository();
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = ProductsRepository
+                .Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+
+    }
+}
